ed35bc93174ee866f4970d3693943ca616aee159
hi-masaki <h69.masaki@gmail.com>
2026-04-26 00:44:22 +0900

    JIS配列への対応

--- src/layouts/layout_dvorak.h
+++
@@ -1,9 +0,0 @@
-#ifdef CONFIG_LAYOUT_SHIFT_TARGET_DVORAK
-#define LAYOUT_DEFINED
-// Dvorak keyboard layout mappings
-// Maps US QWERTY keycodes to their Dvorak equivalents
-static const struct keycode_mapping layout_map[] = {
-    /* from -> to, optional_modifiers */
-    // Add Dvorak-specific mappings here
-};
-#endif
--- src/layouts/layout_jis.h
+++
@@ -1,9 +0,0 @@
-#ifdef CONFIG_LAYOUT_SHIFT_TARGET_JIS
-#define LAYOUT_DEFINED
-// Japanese (JIS) keyboard layout mappings
-// Maps US QWERTY keycodes to their JIS equivalents
-static const struct keycode_mapping layout_map[] = {
-    /* from -> to, optional_modifiers */
-    // Add JIS-specific mappings here
-};
-#endif
